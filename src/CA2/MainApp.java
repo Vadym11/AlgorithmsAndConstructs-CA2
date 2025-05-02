7bc0d840ab40bd8ceba47f8a2b02e5d84ef1ee5a
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2025-05-02 21:29:54 +0100

    sort and search added

--- src/CA2/MainApp.java
+++ src/CA2/MainApp.java
@@ -14,49 +14,11 @@ public class MainApp {
         Manager ITManager = Utils.generateRandomManager(DepartmentNames.IT.getFullName());
         DepartmentIT ITDepartment = new DepartmentIT(ITManager);
 
-//        for (Employee employee : HRDepartment.getEmployees()) {
-//            System.out.println(employee.getFirstName());
-//            System.out.println(employee.getEmail());
-//            System.out.println(employee.getDivisions());
-//            System.out.println(employee.getSalary());
-//        }
-//
-//        for (Division division : HRDepartment.getDivisions()) {
-//            System.out.println(division.getName());
-//        }
-//
-//        System.out.println(HRDepartment.getManager().getEmail());
+        Employee employee = Utils.generateRandomEmployeeCustomName("Baboon", "Healey", HRDepartment, 3333.33);
+        Utils.writeToCSV(List.of(employee));
 
-        System.out.println("IT Department ============================");
+        List<String> found = Utils.searchRecords("Baboon", 0);
 
-        for (Employee employee : HRDepartment.getEmployees()) {
-            System.out.println(employee.getFirstName());
-            System.out.println(employee.getEmail());
-            System.out.println(employee.getDivisionsAsString());
-            System.out.println(employee.getSalary());
-            System.out.println(employee.getJobTitle());
-            System.out.println(employee.getInfo());
-            System.out.println("-----------------------------------");
-        }
-
-
-        for (Division division : HRDepartment.getDivisions()) {
-            System.out.println(division.getName());
-        }
-
-        System.out.println(HRDepartment.getManager().getEmail());
-        System.out.println(HRDepartment.getManager().getDivisionsAsString());
-        System.out.println(HRManager.getDivisionsAsString());
-        System.out.println(HRDepartment.getDivisionsAsString());
-
-        System.out.println(ITManager.getInfo());
-
-//        Utils.writeToCSV(HRDepartment.getEmployees(), "employees.csv");
-//        Utils.writeToCSV(List.of(HRManager, HRManager), "employees.csv");
-//
-//        Utils.writeToCSV(ITDepartment.getEmployees(), "employees.csv");
-//        Utils.writeToCSV(List.of(ITManager, HRManager), "employees.csv");
-        Employee employee = Utils.generateRandomEmployeeCustomName("Ken", "Healey", HRDepartment, 3333.33);
-        Utils.writeToCSV(List.of(employee), "employees.csv");
+        Utils.printCSVAsTable(found);
     }
 }
--- src/CA2/Utils.java
+++ src/CA2/Utils.java
@@ -3,30 +3,46 @@ package CA2;
 import CA2.constants.DepartmentNames;
 import CA2.constants.HRDivisionNames;
 import CA2.constants.ITDivisionNames;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Utils {
 
+    private static final String MY_FILE_NAME = "employees.csv";
+
     static Random random = new Random();
-    static String[] names = {"Abby", "Abdul", "Ada", "Addison", "Adelbert", "Adelina", "Adella", "Adolf", "Adolpho", "Adriane", "Adrianne", "Adrien", "Aigneis", "Ailee", "Alex", "Alice", "Aaron", "Ava"};
-    static String[] lastNames = {"Lulham", "Siaskowski", "Blinkhorn", "Tamburo", "Hartzenberg", "Coniam", "Field", "Carthew", "Ramsey", "Alderton", "Pattle", "Emblow", "Witchell", "Forsdicke", "Chrispin", "Johnson", "Smith", "Williams"};
-    static String[] domains = {"gmail.com", "yahoo.com", "outlook.com", "hotmail.com", "icloud.com", "aol.com", "live.com"};
+
+    static String[] FIRST_NAMES = {
+            "Abby", "Abdul", "Ada", "Addison", "Adelbert", "Adelina", "Adella", "Adolf", "Adolpho", "Adriane",
+            "Adrianne", "Adrien", "Aigneis", "Ailee", "Alex", "Alice", "Aaron", "Ava",
+            "Benjamin", "Bianca", "Cameron", "Cassandra", "Dante", "Diana", "Edward", "Ella", "Felix", "Fiona",
+            "George", "Grace", "Henry", "Hazel", "Isaac", "Ivy", "Jacob", "Jasmine", "Kevin", "Kira",
+            "Leo", "Luna", "Miles", "Mia", "Nathan", "Nora", "Oscar", "Olivia", "Paul", "Piper",
+            "Quinn", "Quincy"
+    };
+
+    static String[] LAST_NAMES = {
+            "Lulham", "Siaskowski", "Blinkhorn", "Tamburo", "Hartzenberg", "Coniam", "Field", "Carthew", "Ramsey", "Alderton",
+            "Pattle", "Emblow", "Witchell", "Forsdicke", "Chrispin", "Johnson", "Smith", "Williams",
+            "Brown", "Taylor", "Davies", "Evans", "Wilson", "Moore", "Anderson", "Thomas", "Jackson", "White",
+            "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson", "Clark", "Rodriguez", "Lewis", "Lee",
+            "Walker", "Hall", "Allen", "Young", "King", "Wright", "Scott", "Torres", "Nguyen", "Murphy",
+            "Kelly", "Reed"
+    };
+
+    static String[] DOMAINS = {"gmail.com", "yahoo.com", "outlook.com", "hotmail.com", "icloud.com", "aol.com", "live.com"};
 
     private static String generateRandomName() {
-        return names[random.nextInt(names.length)];
+        return FIRST_NAMES[random.nextInt(FIRST_NAMES.length)];
     }
 
     private static String generateRandomLastName() {
-        return lastNames[random.nextInt(lastNames.length)];
+        return LAST_NAMES[random.nextInt(LAST_NAMES.length)];
     }
 
     private static String generateRandomDomain() {
-        return domains[random.nextInt(domains.length)];
+        return DOMAINS[random.nextInt(DOMAINS.length)];
     }
 
     private static String generateRandomGender() {
@@ -84,6 +100,7 @@ public class Utils {
                     .map(HRDivisionNames::getDivisionName)
                     .collect(Collectors.toList());
         }
+
         return new Manager(
                 firstName,
                 lastName,
@@ -96,24 +113,172 @@ public class Utils {
                 "Head of Department");
     }
 
-    public static void writeToCSV(List<Employee> users, String filename) {
-        File file = new File(filename);
+    public static void writeToCSV(List<Employee> employees) {
+        File file = new File(MY_FILE_NAME);
         boolean fileExists = file.exists();
 
-        try (FileWriter writer = new FileWriter(filename, true)) { // Append mode = true
+        try (FileWriter writer = new FileWriter(MY_FILE_NAME, true)) { // Append mode = true
             if (!fileExists) {
                 // Write header only if file doesn't exist
                 writer.write("First Name,Last Name,Gender,Email,Salary,Position,Division(s),Department,Job Title\n");
             }
-
             // Write each user's data
-            for (Employee user : users) {
-                writer.write(user.getInfo());
+            for (Employee employee : employees) {
+                writer.write(employee.getInfo());
             }
-            System.out.println("Data successfully written to " + filename);
+            System.out.println("Data successfully written to " + MY_FILE_NAME);
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file.");
             e.printStackTrace();
+        }
+    }
+
+    public static List<String> readFile() {
+        List<String> fileList = new ArrayList<>();
+        try {
+            //must have a tr-catch because the file might not exist
+            BufferedReader myFile = new BufferedReader(new FileReader(MY_FILE_NAME));
+            //there is something in the file. Let's see what it is!
+            String input = myFile.readLine();
+            //now a loop to read all the data from the file
+            //USe while() loop because the file *might* be empty!
+            while (input != null){
+                fileList.add(input);
+                input = myFile.readLine();
+            }
+            System.out.println("Finished reading from File");
+        }catch (Exception e){
+            //error opening file
+            System.out.println("Error - unable to find file " + MY_FILE_NAME);
+        }
+
+        return fileList;
+    }
+
+    public static void insertionSortCustom(List<String> myList, int option) {
+        int pos;
+        String keyElement;
+
+        for (int i = 1; i < myList.size(); i++) {
+            keyElement = myList.get(i);
+            pos = i;
+            while (pos > 0 && (myList.get(pos - 1).split(",")[option])
+                    .compareTo(keyElement.split(",")[option]) > 0) {
+                String elemAtPosMinusOne = myList.get(pos - 1);
+                myList.set(pos, elemAtPosMinusOne);
+                pos = pos - 1;
+            }
+            myList.set(pos, keyElement);
+        }
+    }
+
+    public static int binarySearchRecursiveCustom(List<String> myList, int option, String key, int start, int end) {
+        if (start > end) {
+            return -1; // Key not found
+        }
+
+        int middle = (end + start)/2;
+        String word = myList.get(middle).split(",")[option];
+
+        if (word.compareTo(key) == 0) {
+            return middle;
+        } else if (word.compareTo(key) < 0) {
+            return binarySearchRecursiveCustom(myList, option, key, middle + 1, end);
+        } else {
+            return binarySearchRecursiveCustom(myList, option, key, start, middle - 1);
+        }
+    }
+
+    public static List<Integer> binarySearchAllDuplicates(List<String> myList, int option, String key, int start, int end) {
+        List<Integer> result = new ArrayList<>();
+
+        // First, find one match using binary search
+        int matchIndex = binarySearchRecursiveCustom(myList, option, key, start, end);
+        if (matchIndex == -1) return result;
+
+        // Add the match
+        result.add(matchIndex);
+
+        // Look left
+        int left = matchIndex - 1;
+        while (left >= start) {
+            if (myList.get(left).split(",")[option].equals(key)) {
+                result.add(left);
+                left--;
+            } else {
+                break;
+            }
+        }
+
+        // Look right
+        int right = matchIndex + 1;
+        while (right <= end) {
+            if (myList.get(right).split(",")[option].equals(key)) {
+                result.add(right);
+                right++;
+            } else {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static List<String> searchRecords(String searchQuery, int option) {
+        List<String> recordsFound = new ArrayList<>();
+        List<String> fileList = readFile();
+
+        // add header to the records found
+        recordsFound.add(fileList.get(0));
+        insertionSortCustom(fileList, option);
+
+        List<Integer> foundIndexes = binarySearchAllDuplicates(
+                fileList, option, searchQuery, 0, (fileList.size() - 1));
+
+        for (int index : foundIndexes) {
+            recordsFound.add(fileList.get(index));
+        }
+
+        return recordsFound;
+    }
+
+    public static void printCSVAsTable(List<String> file) {
+        List<String[]> fileArray = new ArrayList<>();
+        for (String elem : file) {
+            fileArray.add(elem.split(","));
+        }
+
+        // Determine column widths
+        int numCols = fileArray.get(0).length;
+        int[] colWidths = new int[numCols];
+        for (String[] row : fileArray) {
+            for (int i = 0; i < numCols; i++) {
+                colWidths[i] = Math.max(colWidths[i], row[i].length());
+            }
+        }
+
+        // Build separator row
+        StringBuilder separator = new StringBuilder();
+        for (int width : colWidths) {
+            separator.append("+").append("-".repeat(width + 2));
+        }
+        separator.append("+");
+        String sepRow = separator.toString();
+
+        // Print top border
+        System.out.println(sepRow);
+
+        // Print rows with formatting
+        for (String[] row : fileArray) {
+            System.out.print("|");
+            for (int i = 0; i < numCols; i++) {
+                System.out.printf(" %-" + colWidths[i] + "s |", row[i]);
+            }
+            System.out.println();
+
+            // Print separator after header and last row
+            System.out.println(sepRow);
         }
     }
 }
